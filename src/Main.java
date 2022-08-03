053a58bac63cf015ef5f702f73233168ca6c7d61
Rafikova Darya <darya.sladkova.89@mail.ru>
2022-08-03 11:28:48 +0400

    добавили long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new int[]{100, 200, 300});
+        SalesManager manager = new SalesManager(new long[]{100, 200, 300});
         System.out.println("Максимальная цена " + manager.max());
 
     }
